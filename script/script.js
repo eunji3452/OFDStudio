f99f6ea2a3bcda0a4a1a08bfd5e84336f887d27a
eunji3452 <82145698+eunji3452@users.noreply.github.com>
2021-09-23 22:38:49 +0900

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,15 @@
+
+    $( '#bgVideo' ).YTPlayer();
+
+    let filters = {
+        grayscale : 80,
+        sepia : 20,
+        opacity : 90
+      }
+      jQuery('#bgVideo').YTPApplyFilters(filters)
+
+
+
+
+    //   schedule
+
