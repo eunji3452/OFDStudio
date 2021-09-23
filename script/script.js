1fafe3556f24c513ab65a92fab83f79f5d8e8b1a
eunji3452 <82145698+eunji3452@users.noreply.github.com>
2021-09-23 23:08:15 +0900

    add script + html
      script level: 1 //지도의 레벨(확대, 축소 정도)
    
    <script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=APIKEY&libraries=LIBRARY"></script>

--- script/script.js
+++ script/script.js
@@ -17,7 +17,7 @@
     let container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
     let options = { //지도를 생성할 때 필요한 기본 옵션
   center: new kakao.maps.LatLng(37.55007564955231, 126.92208312589833), //지도의 중심좌표.37.55007564955231, 126.92208312589833
-  level: 3 //지도의 레벨(확대, 축소 정도)
+  level: 1 //지도의 레벨(확대, 축소 정도)
     };
 
     let map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
